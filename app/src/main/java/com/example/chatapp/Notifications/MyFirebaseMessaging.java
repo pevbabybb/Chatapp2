3ff9f0811b8ec0c151debc925d0eb8a4208fa66b
TrinhHoangNguyen <48852717+TrinhHoangNguyen@users.noreply.github.com>
2019-12-19 22:26:37 +0700

    alo

--- app/src/main/java/com/example/chatapp/Notifications/MyFirebaseMessaging.java
+++ app/src/main/java/com/example/chatapp/Notifications/MyFirebaseMessaging.java
@@ -1,11 +1,13 @@
 package com.example.chatapp.Notifications;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -27,8 +29,40 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         if(firebaseUser !=null && sented.equals(firebaseUser.getUid())){
-            sendNotification(remoteMessage);
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                sendONotification(remoteMessage);
+            }
+else {
+                sendNotification(remoteMessage);
+            }
         }
+    }
+
+    private void  sendONotification(RemoteMessage remoteMessage){
+        String user = remoteMessage.getData().get("user");
+        String icon = remoteMessage.getData().get("icon");
+        String title = remoteMessage.getData().get("title");
+        String body = remoteMessage.getData().get("body");
+
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        int j =  Integer.parseInt(user.replaceAll("[//D]",""));
+        Intent intent = new Intent(this, MessageActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("userID",user);
+        intent.putExtras(bundle);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,j,intent,PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        ONotification oNotification = new ONotification(this);
+        Notification.Builder builder = oNotification.getONotification(title,body,pendingIntent,defaultSound,icon);
+        int i = 0 ;
+        if (j>0){
+            i = j;
+        }
+     oNotification.getManager().notify(i,builder.build());
+
     }
 
     private void sendNotification(RemoteMessage remoteMessage) {

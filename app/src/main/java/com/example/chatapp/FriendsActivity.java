a00f2183ddb2a395d7b40fa7ef1c7cdb2dbea203
TrinhHoangNguyen <48852717+TrinhHoangNguyen@users.noreply.github.com>
2019-12-23 08:49:07 +0700

    last

--- app/src/main/java/com/example/chatapp/FriendsActivity.java
+++ app/src/main/java/com/example/chatapp/FriendsActivity.java
@@ -1,6 +1,7 @@
 package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class FriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Friends");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/chatapp/RequestsActivity.java
+++ app/src/main/java/com/example/chatapp/RequestsActivity.java
@@ -1,6 +1,7 @@
 package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class RequestsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requests);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Request");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
